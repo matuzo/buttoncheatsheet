ae472dfd0d16ccbaab18af2dd7cf9380b6c1d719
Manuel Matuzovic <manuel@matuzo.at>
2021-05-17 19:12:07 +0200

    First draft

---
+++ .eleventy.js
@@ -0,0 +1,27 @@
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function (eleventyConfig) {
+  // Plugins
+  eleventyConfig.addPlugin(syntaxHighlight);
+
+  eleventyConfig.addPassthroughCopy("./src/assets");
+
+  
+  return {
+    templateFormats: ["md", "njk"],
+
+    pathPrefix: "/",
+
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    passthroughFileCopy: true,
+    dir: {
+      input: "src",
+      includes: "_includes",
+      layouts: "_layouts",
+      data: "_data",
+      output: "_site"
+    }
+  };
+};
